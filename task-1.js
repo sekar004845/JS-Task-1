0c2f46b7b566ae8e32126d6644790d88b328e4a7
sekar004845 <sekar004845@gmail.com>
2023-02-06 01:37:57 -0800

    update

---
+++ task-1.js
@@ -0,0 +1,50 @@
+// Arithmetic operator //
+var a=5;
+var b=7;
+console.log(a+b);
+console.log(a-b);
+console.log(b-a);
+console.log(a*b);
+console.log(a/b);
+console.log(b/a);
+console.log(b%a);
+console.log(a%b);
+console.log(a**b);
+console.log(b**a);
+console.log(6+5);
+
+//pre increment//
+console.log(++a);
+//post increment//
+console.log(a++);
+console.log(a);
+//pre decrement//
+console.log(--a);
+// post decrement//
+console.log(a--);
+console.log(a);
+
+// Relational operator //
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+
+// Equality operator //
+var x=8;
+var y="8"
+var z=9;
+var a=9;
+var b="8";
+
+console.log(x==y);
+console.log(x!==y);
+
+console.log(x==z); 
+console.log(x!==z);
+console.log(x===y);
+console.log(a===z);
+console.log(b===y);
+
+console.log(5+7+"6"+8+6);
+console.log(8+"6"-7+8+2);
